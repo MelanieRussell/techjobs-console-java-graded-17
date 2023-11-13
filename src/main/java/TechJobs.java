f300d4a04a31e2bdfde67a7b0c1fd99ff33eddc9
MelanieRussell <140012880+MelanieRussell@users.noreply.github.com>
2023-11-12 18:40:03 -0600

    Update TechJobs.java

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,22 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+
+        if(someJobs.isEmpty()){
+            System.out.println("No Results");
+        } else{
+            for (int i=0;i<someJobs.size();i++) {
+                System.out.println("*****");
+                System.out.println("position type: " + someJobs.get(i).get("position type"));
+                System.out.println("name: " + someJobs.get(i).get("name"));
+                System.out.println("employer: " + someJobs.get(i).get("employer"));
+                System.out.println("location: " + someJobs.get(i).get("location"));
+                System.out.println("core competency: " + someJobs.get(i).get("core competency"));
+                System.out.println("*****");
+                // System.out.println(someJobs.get(i));
+            }
+
+        }
+
     }
 }
